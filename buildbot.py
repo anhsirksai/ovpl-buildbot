da148128991847debba1df9ff177563311310145
Siddharth <siddu.druid@gmail.com>
2014-07-03 18:50:33 +0400

    was using old buildbot code

--- buildbot.py
+++ buildbot.py
@@ -2,18 +2,26 @@
 import custom_logging
 import config
 import ovpl
+import git
 
 import sys
+import os
 from flask import Flask, request, make_response
 
 app = Flask(__name__)
 logger = None
 
+def construct_clone_path(git_hash):
+	return str(os.path.join(os.path.abspath("./repos/"), git_hash))
 
 @app.route("/build", methods=["GET"])
 def build_endpoint():
 	git_hash = request.args.get("hash")
 	git_remote = request.args.get("remote")
+
+
+	logger.info("git hash: {}".format(git_hash))
+	logger.info("git remote: {}".format(git_remote))
 
 	if git_hash == None:
 		return make_response("git hash not sent"), 400
@@ -21,8 +29,16 @@ def build_endpoint():
 		return make_response("git remote not sent"), 400
 
 
-	ovpl_instance = ovpl.OVPLInstance(git_hash, git_remote)
+	git_hash_str = str(git_hash).strip()
+	git_remote_str = str(git_remote).strip()
 
+	git_clone_path = construct_clone_path(git_hash)
+	ovpl_repo = git.Git(logger=logger, remote=git_remote_str, repo_folder_path=git_clone_path)
+	logger.info("cloning succeeded. Now checking out version {}".format(git_hash_str))
+	ovpl_repo.checkout(git_hash_str)
+
+	logger.info("starting OVPL tests...")
+	ovpl_instance = ovpl.OVPL(logger=logger, repo_folder_path=git_clone_path)
 	success = ovpl_instance.test()
 
 	if success:
